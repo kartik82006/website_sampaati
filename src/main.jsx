d0c4ff77c9b0851893d8e83ed4fdf78248c69430
atisss <aatishkumarsahu05012006@gmail.com>
2025-06-28 16:38:27 +0530

    links working

--- src/main.jsx
+++ src/main.jsx
@@ -2,19 +2,15 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
-import ProjectShowcase from './Project.jsx';
-import App from './App.jsx';
-import TeamLeaders from './TeamLeaders.jsx';
+import Home from './Home.jsx';
 import TeamMembers from './TeamMembers.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<TeamLeaders />} />
-        <Route path="/leaders" element={<TeamLeaders />} />
+        <Route path="/" element={<Home />} />
         <Route path="/team/:teamName" element={<TeamMembers />} />
-        <Route path="/projects" element={<ProjectShowcase />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
