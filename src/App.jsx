d15f49d491570637cfeac743f0f0f1bbe4c8321c
kartik82006 <kartikgupta15244.dcmc@gmail.com>
2025-07-14 00:20:33 +0530

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/img1.jpeg'
 import './App.css'
 import Navbar from './Navbar'
 import { TimelineDemo } from './TimelineFloater'
