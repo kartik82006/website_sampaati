03eeecb2edd84d04c68249a0076bc78040fb31df
kartik82006 <kartikgupta15244.dcmc@gmail.com>
2025-08-02 02:09:18 +0530

    small changes

--- src/ui/TimeLine.jsx
+++ src/ui/TimeLine.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+
 const Timeline = ({ items }) => {
   return (
     <section className="relative max-w-4xl mx-auto py-12 px-4">
@@ -15,21 +16,24 @@ const Timeline = ({ items }) => {
             key={index}
             className={`relative w-full mb-10 flex `}
           >{/* Left or Right alignment based on index */}
+
+          
             <div className={`w-full md:w-1/2 px-4 ${isLeft?"":"order-2"}` }>
-              <div className=" p-6 rounded-lg shadow-md bg-[#0B101E]">
+              <div className=" p-6 rounded-lg shadow-md bg-[#0B101E] m-4">
                 <h3 className="text-cyan-600 font-bold mb-2">{item.year}</h3>
                 <p className="text-white">{item.description}</p>
               </div>                                           
             </div>
-
+          
             {/* Dot on line */}
             <div className="absolute top-4 left-1/2 transform -translate-x-1/2 w-5 h-5 bg-cyan-500 border-4 border-white rounded-full shadow-md z-10"></div>
-
+            
           <div className={`w-full md:w-1/2 px-4 ${isLeft?":order-2":""} `}>
-              <div className=" p-6 rounded-lg shadow-md bg-[#0B101E] ">
+              <div className=" p-4 rounded-lg shadow-md bg-[#0B101E] hover:bg-[#1E293B] transition-colors transition-transform hover:scale-105 duration-500 ease-in-out m-2 ">
                 <img src={item.image} alt="image1" />
               </div>
             </div>
+            
 
           </div>
 
