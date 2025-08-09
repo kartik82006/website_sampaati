11c850176ced9ba87d63f7a2b959402b4835748e
kartik82006 <kartikgupta15244.dcmc@gmail.com>
2025-08-09 11:49:09 +0530

    minor changes

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -3,9 +3,11 @@ import React from 'react'
 function Hero() {
   return (
     <div className='p-3 w-full h-[100vh]'>
-        <h1 className='text-5xl mt-50 m-4 w-[50%] z-30 absolute text-white font-bold'>
-            DREAM, DESIGN, DOMINATE THE SKIES!
-        </h1>
+          <h1 className='text-4xl mt-10 m-4 w-[65%] z-30 absolute text-white font-bold'>
+            DREAM. DESIGN. DOMINATE THE SKIES!
+          </h1>
+        
+
     </div>
   )
 }
